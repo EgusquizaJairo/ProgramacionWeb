acbe5ef753b8cbfa71d91e5ca48fc3b4c12cbee6
EgusquizaJairo <u201811623@upc.edu.pe>
2022-03-25 20:39:33 -0500

    Creacion de una clase y objeto Student

---
+++ Semana 1/src/main/java/pe/edu/upc/Progra.java
@@ -0,0 +1,23 @@
+package pe.edu.upc;
+
+import java.util.Iterator;
+
+public class Progra {
+
+	public static void main(String[] args) {
+	
+		Student student=new Student();
+		
+		student.setFirstName("Jairo Egusquiza");
+		
+		System.out.println(student.getFirstName());
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	
+
+	
+	}
+
+}
